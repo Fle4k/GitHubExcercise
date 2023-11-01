8cb8388524d7b598afc6c8e8f57ad33031a27c6c
Shahin shokoui <shahin@shahin.de>
2023-11-01 13:56:40 +0100

    Initial Commit

---
+++ GitHubExcercise/GitHubExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExcerciseApp.swift
+//  GitHubExcercise
+//
+//  Created by Shahin on 01.11.23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
